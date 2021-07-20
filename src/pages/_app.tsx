668a7896d7d33b09a59be1c702241af46d7747fe
tw-nakano <nakano@tailorworks.com>
2021-07-21 07:56:51 +0900

    :clown_face:mswの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  require('../mocks')
+}
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
