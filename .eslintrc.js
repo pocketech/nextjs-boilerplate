d73b0ce3657088488df36cfda6a4db28af7eab51
tw-nakano <nakano@tailorworks.com>
2021-11-13 22:19:35 +0900

    :arrow_up:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,30 @@
 module.exports = {
   root: true,
-  plugins: ['simple-import-sort'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'next/core-web-vitals',
-    'prettier',
-  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: { jsx: true },
   },
+  settings: { react: { version: 'detect' } },
+  env: { es2021: true, browser: true, jest: true, node: true },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'simple-import-sort',
+    'import-access',
+    'unused-imports',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@next/next/recommended',
+    'prettier',
+  ],
   rules: {
     'no-var-requires': 'off',
     // 単なるconsole.logはエラーにする
@@ -20,6 +33,13 @@ module.exports = {
     'no-restricted-syntax': [
       'error',
       { selector: 'TSEnumDeclaration', message: "Don't declare enums" },
+    ],
+    // 変数宣言と return 周辺を改行必須に
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      { blankLine: 'always', prev: '*', next: 'return' },
     ],
     // コールバック関数は必ずarrow-functionにする
     'prefer-arrow-callback': 'error',
@@ -42,13 +62,16 @@ module.exports = {
         checkInlineFunction: true,
       },
     ],
-    // React Hooks のための設定
-    'react-hooks/rules-of-hooks': 'error',
-    // hooksの依存配列の設定
-    'react-hooks/exhaustive-deps': 'warn',
     // <Component {...props}>を許可する
     'react/jsx-props-no-spreading': 'off',
+    'react/no-array-index-key': 'warn',
     'react/no-unescaped-entities': 'off',
+    // React Hooks のための設定
+    'react-hooks/rules-of-hooks': 'error',
+    // hooksの依存配列に空配列を許容する
+    'react-hooks/exhaustive-deps': 'warn',
+    // import可能な範囲に制限をかける
+    'import-access/jsdoc': ['error'],
     'import/newline-after-import': 'error',
     // default-exportを許容しない
     'import/no-default-export': 'error',
@@ -56,13 +79,19 @@ module.exports = {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     // 明示的なanyは許容する
-    '@typescript-eslint/no-expslicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     // 関数の引数や返り値に必ず型をつけるルールを off にする
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     // 型は明示的にtype-importする
     '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+    // unused-imports/no-unused-varsと競合するためoff
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
     // 未使用の変数がある場合エラーにする（デフォルトは warning）
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'unused-imports/no-unused-vars': [
+      'error',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
     // next/linkのchildのaタグの空hrefを許容する
     'jsx-a11y/anchor-is-valid': 'off',
     // 命名に関するルール
@@ -71,10 +100,22 @@ module.exports = {
       { selector: ['typeAlias', 'typeParameter'], format: ['PascalCase'] },
       { selector: ['property', 'parameterProperty', 'method'], format: ['camelCase'] },
       {
-        selector: 'variable',
+        selector: ['variable'],
         types: ['boolean'],
         format: ['PascalCase'],
-        prefix: ['is', 'has', 'should'],
+        prefix: ['is', 'has', 'should', 'can'],
+      },
+      {
+        selector: ['variable'],
+        types: ['string', 'number', 'array'],
+        modifiers: ['const'],
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      {
+        selector: ['typeProperty'],
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['is', 'has', 'should', 'can'],
       },
     ],
   },
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,9 +1,12 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 if (typeof window === 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { server } = require('./server')
+
   server.listen()
 } else {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { worker } = require('./browser')
+
   worker.start()
 }
 export {}
